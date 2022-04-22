2a5c0aa5d42699d612faf735036706b75514d10a
jalucas- <jalucas-@student.42sp.org.br>
2022-04-23 01:52:35 +0200

    adicionado o rush

---
+++ rush00/main.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+
+void	rush(int x, int y);
+
+int	main()
+{	
+	rush(5, 5); // valor de X e Y
+	return (0);
+}
---
+++ rush00/rush03.c
@@ -0,0 +1,50 @@
+#include <unistd.h>
+
+void ft_putchar(char a);
+
+  int error(int x , int y)
+  {
+    if ((x < 0 || y < 0) && (x > 2,147,483,647 || y > 2,147,483,647))
+	{
+		write(1, "error", 5);
+		return 0;
+	}
+  }
+
+void rush(int x , int y) 
+{
+
+  int c;
+  int l;
+
+  c = 1;
+  l = 1;
+if(error(x, y) != 0)
+{
+	while(l <=y )	{
+		c= 1;
+		while(c <= x)
+		{
+		if((c == 1 && l== 1) || (c == 1 && l == y))
+			ft_putchar('A');
+
+		else if((c == x && l == y) || (c == x && l == 1))
+			ft_putchar('C');
+
+		else if((l == 1 || l == y) || (c == 1 || c == x))
+			ft_putchar('B');
+
+		else
+		{
+			ft_putchar(' ');
+		}
+
+		c++; 
+		}  
+		l ++;
+
+		ft_putchar('\n'); 
+		}
+	}
+
+}
